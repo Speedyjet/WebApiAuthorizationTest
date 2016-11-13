dfa3238a40f963efc670f13b98d818b37a58b5f2
speedyjet <speedyjet@ua.fm>
2016-11-14 01:09:01 +0200

    new project added

---
+++ WebApiAuthorizationTest/WebApiAuthorizationTest/MyAuthorizationServerProvider.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.Owin.Security.OAuth;
+
+namespace WebApiAuthorizationTest
+{
+    public class MyAuthorizationServerProvider: OAuthAuthorizationServerProvider
+    {
+        public override async Task ValidateClientAuthentication(OAuthValidateClientAuthenticationContext context)
+        {
+            context.Validated();
+        }
+    }
+}
